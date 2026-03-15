48d366fb4623e860ddc792f8cfc431dcca293a23
Fortunata <alinka.makarova.2000@bk.ru>
2026-03-16 00:09:55 +0300

    сделанный проект

---
+++ Desktop/django_sprint3-main/blogicum/blog/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from blog.models import Category, Post
+from datetime import datetime
+
+
+# Create your views here.
+def index(request):
+    template_name = 'blog/index.html'
+    post_list = Post.objects.filter(
+        is_published=True, 
+        category__is_published=True,
+        pub_date__lte=datetime.now())[0:5]
+
+    context = {
+        'post_list': post_list
+    }
+    return render(request, template_name, context)
+
+
+def post_detail(request, post_id):
+    template_name = 'blog/detail.html'
+    post = get_object_or_404(
+        Post, 
+        id=post_id, 
+        is_published=True,
+        pub_date__lte=datetime.now(),
+        category__is_published=True)
+
+    context = {
+        'post': post
+    }
+
+    return render(request, template_name, context)
+
+
+def category_posts(request, category_slug):
+    template_name = 'blog/category.html'
+    category = get_object_or_404(Category, slug=category_slug,
+                                 is_published=True)
+
+    post_list = Post.objects.filter(
+        category=category,
+        is_published=True,
+        pub_date__lte=datetime.now()
+    )
+
+    context = {
+        'category': category,
+        'post_list': post_list
+    }
+    return render(request, template_name, context)
